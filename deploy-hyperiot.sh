45f71c0000f01f8477345c6f6559cf2f4e1f1558
Jonatan Gall <jonatangd.souza@gmail.com>
2019-08-25 16:25:43 -0300

    git ignore, fix deploy script

--- deploy-hyperiot.sh
+++ deploy-hyperiot.sh
@@ -12,7 +12,7 @@ export FABRIC_VERSION=hlfv12
 ./downloadFabric.sh
 ./startFabric.sh
 
-composer card delete -c PeerAdmin@fabric-network
+composer card delete -c PeerAdmin@hyperiot-network
 composer card delete -c admin@hyperiot-network
 
 echo .
@@ -20,6 +20,12 @@ echo "[ Hyper iot ] ==> create card"
 echo .
 
 composer card create -p hyperiot/connection.json -u PeerAdmin -c hyperiot/Admin@org1.example.com-cert.pem -k hyperiot/114aab0e76bf0c78308f89efc4b8c9423e31568da0c340ca187a9b17aa9a4457_sk -r PeerAdmin -r ChannelAdmin
+
+echo .
+echo "[ Hyper iot ] ==> Import admin card"
+echo .
+composer card import -f PeerAdmin@hyperiot-network.card
+
 
 echo .
 echo "[ Hyper iot ] ==> install bna network"
